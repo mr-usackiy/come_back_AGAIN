b044ec1f48404daa18ab93d0ed556a3fc677150b
mr-usackiy <77061592+mr-usackiy@users.noreply.github.com>
2022-01-05 17:39:59 +0300

    05.01.21
    Выравнил все по одному wrapper.
    Создал анимацию навигационного бара .

--- js/script.js
+++ js/script.js
@@ -1,23 +1,50 @@
 
-$(document).ready(function(){ 
-            $('.nav_search_text').click(function(){
-            $('.popup_search').toggleClass('visible');
+$(document).ready(function () {
+    $('.nav_search_text').click(function () {
+        $('.popup_search').toggleClass('visible');
+    })
+
+
+
+
+    $(window).scroll(function () {
+        var st = $(this).scrollTop();
+        $('.intro_content').css({
+            "transform": "translate(0%, -" + st / 8 + "%"
         });
+    });
+
+
+    $('.red_more').click(function () {
+        $('.content_wrap-2').css('background-image', 'url(/img/bromo_2.jpg)');
+    });
 
 
 
-        
-        $(window).scroll(function(){
-            var st = $(this).scrollTop();
-            $('.intro_content').css({
-                "transform" : "translate(0%, -" + st/8 + "%"
-            });
+    const down = document.querySelector('.down');
+    const header_nav = document.querySelector('.header_nav');
+
+
+    const headerFixed = () => {
+        window.addEventListener('scroll', () => {
+            let scrollTop = window.scrollY;
+            let downCenter = down.offsetHeight /2;
+
+            if (scrollTop >= downCenter) {
+                $('.header_nav').addClass('fixed')
+                down.style.marginTop = '${header_nav.offsetHeight}px';
+            } else {
+                header_nav.classList.remove('fixed')
+                down.style.marginTop = '0px';
+            }
+
         });
+    };
 
-
-        $('.red_more').click(function(){
-            $('.content_wrap-2').css('background-image', 'url(/img/bromo_2.jpg)');
-        });
+headerFixed();
+window.addEventListener('scroll', () => {
+    headerFixed();
+});
 })
 
 
